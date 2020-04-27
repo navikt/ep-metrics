d9d4d6e1f4e08785069b44ce33cd22d10825b3cf
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2020-04-27 22:20:05 +0200

    Add MetricsHelper

---
+++ src/main/kotlin/no/nav/eessi/pensjon/metrics/MetricsHelper.kt
@@ -0,0 +1,82 @@
+package no.nav.eessi.pensjon.metrics
+
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
+import no.nav.eessi.pensjon.metrics.MetricsHelper.Metric
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class MetricsHelper(val registry: MeterRegistry, @Autowired(required = false) val configuration: Configuration = Configuration()) {
+
+    fun init(method: String,
+             meterName: String = configuration.measureMeterName,
+             success: String = configuration.successTypeTagValue,
+             failure: String = configuration.failureTypeTagValue) = Metric(method, meterName, success, failure)
+
+    inner class Metric(
+            private val method: String,
+            private val meterName: String = configuration.measureMeterName,
+            private val success: String = configuration.successTypeTagValue,
+            private val failure: String = configuration.failureTypeTagValue) {
+
+        /**
+         * Alle counters må legges inn i init'es lik at counteren med konkrete tagger blir initiert med 0.
+         * Dette er nødvendig for at grafana alarmer skal fungere i alle tilfeller.
+         */
+        init {
+            Counter.builder(meterName)
+                    .tag(configuration.typeTag, success)
+                    .tag(configuration.methodTag, method)
+                    .register(registry)
+
+            Counter.builder(meterName)
+                    .tag(configuration.typeTag, failure)
+                    .tag(configuration.methodTag, method)
+                    .register(registry)
+        }
+
+        fun <R> measure(block: () -> R): R {
+
+            var typeTag = success
+
+            try {
+                return Timer.builder("$meterName.${configuration.measureTimerSuffix}")
+                        .tag(configuration.methodTag, method)
+                        .register(registry)
+                        .recordCallable {
+                            block.invoke()
+                        }
+            } catch (throwable: Throwable) {
+                typeTag = failure
+                throw throwable
+            } finally {
+                try {
+                    Counter.builder(meterName)
+                            .tag(configuration.methodTag, method)
+                            .tag(configuration.typeTag, typeTag)
+                            .register(registry)
+                            .increment()
+                } catch (e: Exception) {
+                    // ignoring on purpose
+                }
+            }
+        }
+    }
+
+
+    data class Configuration(
+            val incrementMeterName: String = "event",
+            val measureMeterName: String = "method",
+            val measureTimerSuffix: String = "timer",
+
+            val eventTag: String = "event",
+            val methodTag: String = "method",
+            val typeTag: String = "type",
+
+            val successTypeTagValue: String = "successful",
+            val failureTypeTagValue: String = "failed"
+    )
+}
---
+++ src/test/kotlin/no/nav/eessi/pensjon/metrics/MetricsHelperTest.kt
@@ -0,0 +1,106 @@
+package no.nav.eessi.pensjon.metrics
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.MockClock
+import io.micrometer.core.instrument.simple.SimpleConfig
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.IOError
+import java.util.concurrent.TimeUnit
+
+internal class MetricsHelperTest {
+
+    lateinit var registry: MeterRegistry
+    lateinit var metricsHelper: MetricsHelper
+    lateinit var config: MetricsHelper.Configuration
+
+    @BeforeEach
+    fun setup() {
+        registry = SimpleMeterRegistry()
+        config = MetricsHelper.Configuration()
+        metricsHelper = MetricsHelper(registry)
+    }
+
+    @Test
+    fun measure_counts_success_up_by_one_if_no_exception() {
+        val dummy = metricsHelper.init("dummy")
+        dummy.measure {
+            // the logic you want measured - no exception happening her today
+        }
+
+        assertEquals(
+                1.0,
+                registry.counter(
+                        config.measureMeterName,
+                        config.methodTag, "dummy",
+                        config.typeTag, config.successTypeTagValue)
+                        .count(),
+                0.0001)
+    }
+
+    @Test
+    fun measure_counts_failure_up_by_one_if_exception_thrown() {
+        val dummy = metricsHelper.init("dummy")
+        try {
+            dummy.measure {
+                throw RuntimeException("boom!")
+            }
+        } catch (ex: RuntimeException) {
+            // ignoring on purpose
+        }
+
+        assertEquals(
+                1.0,
+                registry.counter(
+                        config.measureMeterName,
+                        config.methodTag, "dummy",
+                        config.typeTag, config.failureTypeTagValue)
+                        .count(),
+                0.0001)
+    }
+
+    @Test
+    fun measure_counts_failure_up_by_one_if_error_thrown() {
+        val dummy = metricsHelper.init("dummy")
+        try {
+            dummy.measure {
+                throw IOError(RuntimeException())
+            }
+        } catch (ex: IOError) {
+            // ignoring on purpose
+        }
+
+        assertEquals(
+                1.0,
+                registry.counter(
+                        config.measureMeterName,
+                        config.methodTag, "dummy",
+                        config.typeTag, config.failureTypeTagValue)
+                        .count(),
+                0.0001)
+    }
+
+    @Test
+    fun measure_registers_a_timer_too() {
+        val mockClock = MockClock()
+        val registry = SimpleMeterRegistry(SimpleConfig.DEFAULT, mockClock)
+        val metricsHelper = MetricsHelper(registry)
+
+        val dummy = metricsHelper.init("dummy")
+        dummy.measure {
+            // the logic you want counted - no exception happening her today
+            mockClock.add(100, TimeUnit.MILLISECONDS)
+        }
+
+        val timer = registry.timer(
+                "${config.measureMeterName}.${config.measureTimerSuffix}",
+                config.methodTag, "dummy")
+        assertEquals(1, timer.count())
+        assertEquals(
+                100.0,
+                timer.totalTime(TimeUnit.MILLISECONDS),
+                10.0)
+    }
+}
