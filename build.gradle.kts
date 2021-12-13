3745dad65afbcef45e58895b64fcae6a6d8c6597
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-12-13 15:41:49 +0100

    Bumper versjoner og gradle versjon 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    id("org.jetbrains.kotlin.jvm") version "1.6.0"
     `java-library`
     id("net.researchgate.release") version "2.8.1"
     `maven-publish`
-    id("com.adarshr.test-logger") version "2.0.0"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.5.21"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("se.patrikerdes.use-latest-versions") version "0.2.14"
+    id("com.adarshr.test-logger") version "3.1.0"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.6.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("org.owasp.dependencycheck") version "6.5.0.1"
 }
 
@@ -38,8 +38,7 @@ val junitVersion by extra("5.8.+")
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.7.2")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.1")
     implementation("org.springframework:spring-web:$springVersion")
     implementation("org.springframework:spring-context:$springVersion")
     compileOnly("javax.servlet:javax.servlet-api:4.0.1")
@@ -47,7 +46,7 @@ dependencies {
     testImplementation("javax.servlet:javax.servlet-api:4.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
     testImplementation("org.springframework:spring-test:$springVersion")
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("io.mockk:mockk:1.12.1")
 }
 
 // https://github.com/researchgate/gradle-release
